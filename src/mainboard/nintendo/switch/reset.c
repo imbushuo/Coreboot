d1af203f7e974889ec37c2604150f7a8331e6b88
Andre Heider <a.heider@gmail.com>
2018-02-03 13:34:52 +0100

    reset/die now enter rcm

--- src/mainboard/nintendo/switch/reset.c
+++ src/mainboard/nintendo/switch/reset.c
@@ -14,11 +14,29 @@
  */
 
 #include <arch/io.h>
+#include <console/console.h>
 #include <reset.h>
 
-#include "gpio.h"
+#define PMC_CTLR_BASE		(void *)(0x7000e400)
+#define PMC_CNTRL		0x000
+#define  PMC_CNTRL_MAIN_RST	(1 << 4)
+#define PMC_SCRATCH0		0x050
+#define  PMC_SCRATCH0_MODE_RCM	(1 << 1)
 
 void do_hard_reset(void)
 {
-	gpio_output(AP_SYS_RESET_L, 0);
+	uint32_t val;
+	
+	val = read32(PMC_CTLR_BASE + PMC_SCRATCH0);
+	val |= PMC_SCRATCH0_MODE_RCM;
+	write32(PMC_CTLR_BASE + PMC_SCRATCH0, val);
+
+	val = read32(PMC_CTLR_BASE + PMC_CNTRL);
+	val |= PMC_CNTRL_MAIN_RST;
+	write32(PMC_CTLR_BASE + PMC_CNTRL, val);
+}
+
+void die_notify(void)
+{
+	do_hard_reset();
 }
